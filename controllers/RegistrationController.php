00b0438683ec8d00d65c993203c1683b1e79dfb0
ScarlettJay <121699563+ScarlettJay@users.noreply.github.com>
2024-01-07 03:07:01 +0800

    Add files via upload

---
+++ controllers/RegistrationController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace PHPMaker2022\project1;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class RegistrationController extends ControllerBase
+{
+    // list
+    public function list(Request $request, Response $response, array $args): Response
+    {
+        return $this->runPage($request, $response, $args, "RegistrationList");
+    }
+
+    // add
+    public function add(Request $request, Response $response, array $args): Response
+    {
+        return $this->runPage($request, $response, $args, "RegistrationAdd");
+    }
+
+    // edit
+    public function edit(Request $request, Response $response, array $args): Response
+    {
+        return $this->runPage($request, $response, $args, "RegistrationEdit");
+    }
+
+    // delete
+    public function delete(Request $request, Response $response, array $args): Response
+    {
+        return $this->runPage($request, $response, $args, "RegistrationDelete");
+    }
+}
